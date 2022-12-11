710210209bdb63d862ffd84b8a61396dbd570748
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2022-12-11 13:14:32 +0300

    Final commit
    Hopefully

--- genetic_algorithm.py
+++ genetic_algorithm.py
@@ -323,6 +323,127 @@ len(stores)
 population = build_population(200, 165)
 len(population[0])
 
+
+# ### 8.1 Building the final algorithm
+# The code to build the algorithm has to be re-run with the above data structures altered.
+
+def fitness_func(solution, solution_idx):
+    # loop through the length of the chromosome finding the distance between each
+    total_dist = 0
+
+    for gene in range(0, len(solution)):
+
+        # get the lon lat of the two points
+        a = genes.get(stores[solution[gene]])
+
+        try:
+            b = genes.get(stores[solution[gene + 1]])
+
+            # find the distance (crow flies)
+            dist = geodesic(a, b).kilometers
+
+        except IndexError:
+            dist = 0
+
+        total_dist += dist
+
+    # to optimise this value in the positive direction the inverse of dist is used
+    fitness = 1 / total_dist
+
+    return fitness
+
+
+def pmx_crossover(parent1, parent2, sequence_start, sequence_end):
+    # initialise a child
+    child = np.zeros(parent1.shape[0])
+
+    # get the genes for parent one that are passed on to child one
+    parent1_to_child1_genes = parent1[sequence_start:sequence_end]
+
+    # get the position of genes for each respective combination
+    parent1_to_child1 = np.isin(parent1, parent1_to_child1_genes).nonzero()[0]
+
+    for gene in parent1_to_child1:
+        child[gene] = parent1[gene]
+
+    # gene of parent 2 not in the child
+    genes_not_in_child = parent2[np.isin(parent2, parent1_to_child1_genes, invert=True).nonzero()[0]]
+
+    if genes_not_in_child.shape[0] >= 1:
+        for gene in genes_not_in_child:
+            if gene >= 1:
+                lookup = gene
+                not_in_sequence = True
+
+                while not_in_sequence:
+                    position_in_parent2 = np.where(parent2 == lookup)[0][0]
+
+                    if position_in_parent2 in range(sequence_start, sequence_end):
+                        lookup = parent1[position_in_parent2]
+
+                    else:
+                        child[position_in_parent2] = gene
+                        not_in_sequence = False
+
+    return child
+
+
+def crossover_func(parents, offspring_size, ga_instance):
+    offspring = []
+    idx = 0
+    while len(offspring) != offspring_size[0]:
+        # locate the parents
+        parent1 = parents[idx % parents.shape[0], :].copy()
+        parent2 = parents[(idx + 1) % parents.shape[0], :].copy()
+
+        # find gene sequence in parent 1
+        sequence_start = random.randint(1, parent1.shape[0] - 4)
+        sequence_end = random.randint(sequence_start, parent1.shape[0] - 1)
+
+        # perform crossover
+        child1 = pmx_crossover(parent1, parent2, sequence_start, sequence_end)
+        child2 = pmx_crossover(parent2, parent1, sequence_start, sequence_end)
+
+        offspring.append(child1)
+        offspring.append(child2)
+
+        idx += 1
+
+    return np.array(offspring)
+
+
+def mutation_func(offspring, ga_instance):
+    for chromosome_idx in range(offspring.shape[0]):
+        # define a sequence of genes to reverse
+        sequence_start = random.randint(1, offspring[chromosome_idx].shape[0] - 2)
+        sequence_end = random.randint(sequence_start, offspring[chromosome_idx].shape[0] - 1)
+
+        genes = offspring[chromosome_idx, sequence_start:sequence_end]
+
+        # start at the start of the sequence assigning the reverse sequence back to the chromosome
+        index = 0
+        if len(genes) > 0:
+            for gene in range(sequence_start, sequence_end):
+                offspring[chromosome_idx, gene] = genes[index]
+
+                index += 1
+
+        return offspring
+
+
+def on_crossover(ga_instance, offspring_crossover):
+    # apply mutation to ensure uniqueness
+    offspring_mutation = mutation_func(offspring_crossover, ga_instance)
+
+    # save the new offspring set as the parents of the next generation
+    ga_instance.last_generation_offspring_mutation = offspring_mutation
+
+
+def on_generation(ga):
+    print("Generation", ga.generations_completed)
+    print(ga.population)
+
+
 ga_instance = pygad.GA(num_generations=100,
                        num_parents_mating=40,
                        fitness_func=fitness_func,
